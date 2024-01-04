6492ea9f3803a9fdf29bcf63ffa1ba4eb8b44630
Michael Gonzalez <michaelgonzalez0723@gmail.com>
2024-01-04 18:15:25 -0500

    deleted a folder called assest. Was not used at all

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import './assests/main.css'
+
 
 
 function Contact(){
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './assests/main.css'
 import App from './App';
 import './index.css'
 
