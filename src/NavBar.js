55e4f39263ce5ad0dad10494484ab0310b41545e
Michael Gonzalez <michaelgonzalez0723@gmail.com>
2024-01-09 23:13:44 -0500

    css changes

--- src/NavBar.js
+++ src/NavBar.js
@@ -11,21 +11,21 @@ function NavBar(){
 
 
 return(
-<div>
+<div className="bg-amber-500">
     <img src="https://t3.ftcdn.net/jpg/00/79/67/48/360_F_79674837_Jn4HbqZGciL1jMeobbM6xNOUUjen936J.jpg" alt="Company logo" />  
      
-     <div onClick={handleClick} className="">
+     <div onClick={handleClick}>
          {!nav ?<FaBars/>: <FaTimes/>}
      </div>
      
-    <p>
-        <Link to="/">
+    <p >
+        <Link to="/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">
             Home
         </Link>
 
     </p>
     <p>
-        <Link to="Contact">
+        <Link to="Contact" className="text-blue-600 hover:text-blue-800 visited:text-purple-600">
             Contact
         </Link>
     </p>
