cf814a832e28f48905522ca1b5151f8e9c59b1ce
Michael Gonzalez <michaelgonzalez0723@gmail.com>
2024-01-16 16:12:36 -0500

    made it to where the two divs are next to each other, making it a whole page

--- src/NavBar.js
+++ src/NavBar.js
@@ -13,25 +13,20 @@ function NavBar(){
 return(
 <div className="bg-amber-600 ">
     <img src="https://t3.ftcdn.net/jpg/00/79/67/48/360_F_79674837_Jn4HbqZGciL1jMeobbM6xNOUUjen936J.jpg" alt="Company logo" className="h-auto max-w-xs bg-gradient-to-tl from-blue-500 to-cyan-500" />  
-        
-     <div onClick={handleClick}>
+        <div onClick={handleClick}>
          {!nav ?<FaBars/>: <FaTimes/>}
-     </div>
-     
-    <p >
-        <Link to="/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 w-full h-[80px] flex justify-between items-center hidden md:flex ">
+      </div>
+       <p>
+         <Link to="/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 w-full h-[80px] flex justify-between items-center hidden md:flex ">
             Home
-        </Link>
+         </Link>
 
-    </p>
-    <p>
-        <Link to="Contact" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 w-full h-[80px] flex justify-between items-center hidden md:flex ">
+      </p>
+       <p>
+         <Link to="Contact" className="text-blue-600 hover:text-blue-800 visited:text-purple-600 w-full h-[80px] flex justify-between items-center hidden md:flex ">
             Contact
-        </Link>
-    </p>
-    <p>
-        <Link to="ProfessionalServices"></Link>
-    </p>
+         </Link>
+      </p>
 </div>
 
 )
--- src/ProfessionalServices.js
+++ src/ProfessionalServices.js
@@ -10,40 +10,40 @@ function ProfessionalServices(){
         
         <div className="bg-gray-300">
            <h3 className="font-bold text-6xl">Meet Your Home Service Experts</h3>
-           <img className="h-full"src="https://le-cdn.hibuwebsites.com/64b7818a0493479cbd6724b436d6f79f/dms3rep/multi/opt/694903-1152w.jpg" alt="wrench being held" />
-           <ul className="w-48 text-sm font-medium text-gray-900 bg-amber-600  dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-             <li className=" w-xl  border-gray-200 rounded-t-lg dark:border-gray-600">
+             <img className="max-w-full ms-auto absolute right-0 h-5/6 flex flex-row" src="https://le-cdn.hibuwebsites.com/64b7818a0493479cbd6724b436d6f79f/dms3rep/multi/opt/694903-1152w.jpg" alt="wrench being held" />          
+           <ul className="w-6/12 text-sm font-medium text-gray-900 bg-amber-600  dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
+             <li className="border-gray-200 rounded-t-lg dark:border-gray-600">
                <div className="flex items-center ps-3">
-                   <input id="vue-checkbox" type="checkbox" value="" className="w-11 h-20 text-blue-600 bg-gray-100"></input>
+                   <input id="vue-checkbox" type="checkbox" value="" className="w-11 h-40  text-blue-600 bg-gray-100"></input>
                    <label for="vue-checkbox" className="font-bold text-xl">Emergency Repairs</label>
                </div>
             </li>
            <li className="w-lg  border-gray-200 rounded-t-lg dark:border-gray-600">
                <div className="flex items-center ps-3">
-                    <input id="vue-checkbox" type="checkbox" value=""className="w-9 h-20 text-blue-600 bg-gray-100"></input>
+                    <input id="vue-checkbox" type="checkbox" value=""className="w-9 h-40 text-blue-600 bg-gray-100"></input>
                     <label for="vue-checkbox" className="font-bold text-xl"> Electrical Repair</label>
                 </div> 
             </li>
            <li className="w-lg border-gray-200 rounded-t-lg dark:border-gray-600">
                <div className="flex items-center ps-3">
-                    <input id="vue-checkbox" type="checkbox" value=""className="w-9 h-20 text-blue-600 bg-gray-100"></input>
+                    <input id="vue-checkbox" type="checkbox" value=""className="w-9 h-40 text-blue-600 bg-gray-100"></input>
                     <label for="vue-checkbox" className="font-bold text-xl">Circuit Breakers</label>
                </div> 
            </li>
            <li className="w-lg  border-gray-200 rounded-t-lg dark:border-gray-600">
                <div className="flex items-center ps-3">
-                   <input id="vue-checkbox" type="checkbox" value=""className="w-7 h-20 text-blue-600 bg-gray-100"></input>
+                   <input id="vue-checkbox" type="checkbox" value=""className="w-9 h-40 text-blue-600 bg-gray-100"></input>
                    <label for="vue-checkbox" className="font-bold text-xl">Ceiling Fans</label>
              </div> 
            </li>
            <li className="w-lg  border-gray-200 rounded-t-lg dark:border-gray-600">
                <div className="flex items-center ps-3">
-                    <input id="vue-checkbox" type="checkbox" value=""className="w-7 h-20 text-blue-600 bg-gray-100"></input>
+                    <input id="vue-checkbox" type="checkbox" value=""className="w-9 h-40 text-blue-600 bg-gray-100"></input>
                     <label for="vue-checkbox" className="font-bold text-xl"> Generators</label>
              </div> 
            </li>
         </ul>
-        <button className=" h-12 w-48 outline outline-2 text-lg ">Contact Us</button>
+        <button className=" h-11 w-48 outline outline-2 text-lg ">Contact Us</button>
     </div>
     )
 }
