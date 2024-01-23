c95bcdb5622e84c35b3bfe6b97b2a78b52adc241
Michael Gonzalez <michaelgonzalez0723@gmail.com>
2024-01-23 16:01:46 -0500

    css changes

--- src/component/ProfessionalServices.js
+++ src/component/ProfessionalServices.js
@@ -5,45 +5,51 @@ import React from "react";
 function ProfessionalServices(){
    
     //had a problem with github where its error was object file is empty. What I did to solve that was
+    // to check status on git find .git/objects/ -type f -empty | xargs rm, then to fetch the missing objects using
+    // git fetch -p 
+    //then finally use fsck --full, this command will do a full object store check
+    // definitely use a backup remote copy of your git repo somewhere 
+    // the command to make was cp -a .git .git-old
+    
 
     return(
         
-        <div className="bg-gray-300">
+        <div className="bg-gray-300 py-16">
            <h3 className="font-bold text-6xl">Meet Your Home Service Experts</h3>
              <img className="max-w-full ms-auto absolute right-0 h-5/6 flex flex-row" src="https://le-cdn.hibuwebsites.com/64b7818a0493479cbd6724b436d6f79f/dms3rep/multi/opt/694903-1152w.jpg" alt="wrench being held" />          
            <ul className="w-6/12 text-sm font-medium text-gray-900 bg-gray-300  dark:bg-gray-700 dark:border-gray-600 dark:text-white ">
              <li className="border-gray-200 rounded-t-lg dark:border-gray-600">
                <div className="flex items-center ps-3">
-                   <input id="vue-checkbox" type="checkbox" value="" className="w-11 h-40  text-blue-600 bg-gray-100"></input>
+                   <input id="vue-checkbox" type="checkbox" value="" className="w-11 h-40  text-zinc-600 bg-gray-100"></input>
                    <label htmlFor="vue-checkbox" className="font-bold text-xl">Emergency Repairs</label>
                </div>
             </li>
            <li className="w-lg  border-gray-200 rounded-t-lg dark:border-gray-600">
                <div className="flex items-center ps-3">
-                    <input id="vue-checkbox" type="checkbox" value=""className="w-9 h-40 text-blue-600 bg-gray-100"></input>
+                    <input id="vue-checkbox" type="checkbox" value=""className="w-9 h-40 text-zinc-600 bg-gray-100"></input>
                     <label htmlFor="vue-checkbox" className="font-bold text-xl"> Electrical Repair</label>
                 </div> 
             </li>
            <li className="w-lg border-gray-200 rounded-t-lg dark:border-gray-600">
                <div className="flex items-center ps-3">
-                    <input id="vue-checkbox" type="checkbox" value=""className="w-9 h-40 text-blue-600 bg-gray-100"></input>
+                    <input id="vue-checkbox" type="checkbox" value=""className="w-9 h-40 text-zinc-600 bg-gray-100"></input>
                     <label htmlFor="vue-checkbox" className="font-bold text-xl">Circuit Breakers</label>
                </div> 
            </li>
            <li className="w-lg  border-gray-200 rounded-t-lg dark:border-gray-600">
                <div className="flex items-center ps-3">
-                   <input id="vue-checkbox" type="checkbox" value=""className="w-9 h-40 text-blue-600 bg-gray-100"></input>
+                   <input id="vue-checkbox" type="checkbox" value=""className="w-9 h-40 text-zinc-600 bg-gray-100"></input>
                    <label htmlFor="vue-checkbox" className="font-bold text-xl">Ceiling Fans</label>
              </div> 
            </li>
            <li className="w-lg  border-gray-200 rounded-t-lg dark:border-gray-600">
                <div className="flex items-center ps-3">
-                    <input id="vue-checkbox" type="checkbox" value=""className="w-9 h-40 text-blue-600 bg-gray-100"></input>
+                    <input id="vue-checkbox" type="checkbox" value=""className="w-9 h-40 text-zinc-600 bg-gray-100"></input>
                     <label htmlFor="vue-checkbox" className="font-bold text-xl"> Generators</label>
              </div> 
            </li>
         </ul>
-        <button className=" h-12 w-44 outline outline-2 text-lg ">Contact Us</button>
+        <button className=" h-20 w-48 outline outline-2 text-lg ">Contact Us</button>
     </div>
     )
 }
--- src/component/Reviews.js
+++ src/component/Reviews.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import {AiFillStar, aiFillStar} from 'react-icons/ai'
+import {AiFillStar} from 'react-icons/ai'
 
 function Reviews(){
 const [review,setReview] = useState([]);
@@ -13,29 +13,35 @@ useEffect(()=>{
     .then(json => setReview(json))
     .catch(error => console.log(error));
 },[])
-console.log("review:",review[2])
 
 
 
 
-console.log("starData:",starData)
 // spread operator  === "..."
 
 
  // ffc107 is going to be yellow color 
 
     return(
-        <div className="bg-zinc-900 py-20 flex-wrap flex gap-x-12">
-            {/* <h1 className="font-bold text-6xl text-gray-200">Our Reviews</h1> */}
+        <div className="bg-zinc-900 py-20 ">
+            <h1 className="font-bold text-7xl text-gray-200 py-16">Our Reviews</h1>
             {review.map(reviews => ( 
-                <div className="w-[340px] shadow-lg bg-gray-200" key={reviews.id}>
+                <div className="w-[394px] shadow-lg bg-gray-200 flex flex-wrap py-16 gap-x-12" key={reviews.id}>
                     <h2 className="font-bold text-2xl text-center">{reviews.name}</h2>
-                    <p className="text-lg italic ">{reviews.reviews}</p>
-                    <p>{reviews.rating}</p>
-                    <AiFillStar
-                    className="star"
-                    color="#ffc107"
-                    />
+                    <p className="text-lg italic">{reviews.reviews}</p>
+                    {[...Array(5)].map((star, index)=>{
+                        return(
+                            <label>
+                            <AiFillStar
+                            size={20}
+                            color="#ffc107"/>
+                            </label>
+                            
+                            
+
+                        );
+                    })}
+                    
                 </div>
             ))}
           
