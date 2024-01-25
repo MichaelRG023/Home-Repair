e8e37fb49dcd0736ea5a965a821416ad1668db34
Michael Gonzalez <michaelgonzalez0723@gmail.com>
2024-01-25 15:00:43 -0500

    css changes for better looking ui

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -11,17 +11,17 @@ function Contact(){
 
     return(
         <div className="bg-gray-300">
-            <h1 className="text-gray-900 text-xl">Send us a message</h1>
-                <p className="text-gray-800 px-1 max-w-md">Email us using our simple form and we'll respond within 10 working days</p>
-                <form className="">
-                <input value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder="type out your email..."></input>
+            <h1 className="text-slate-950 text-3xl">Send us a message</h1>
+                <p className="text-gray-800 px-1 max-w-md">Email us using our simple form and we'll respond within 10 working days!</p>
+                <form className>
+                <input value={email} className="w-2/12" onChange={e => setEmail(e.target.value)} type="email" placeholder="type out your email..."></input>
                 </form>
                 <button className="h-12 px-6 m-2 text-lg  transition-colors duration-150 bg-yellow-500 rounded-lg focus:shadow-outline hover:bg-yellow-400">Submit</button>
             
-            <h1 className="text-gray-900 text-xl">Make a Complaint</h1>
-            <p className="text-gray-800 py-3 max-w-md">If you're not happy with the service you've received from Our Company, call us or send us an email and let us know.</p>
+            <h1 className="text-slate-900 text-3xl">Make a Complaint</h1>
+            <p className="text-gray-800 py-3 max-w-md">If you're not happy with the service you've received from Our Company, call us or send us an email and let us know!</p>
             <form>
-            <textarea type="text" id="message" name="text" placeholder="send a complaint..." className="block p-2.5 text-sm text-gray-900  w-2/12 h-16"></textarea>
+            <textarea type="text" id="message" name="message" placeholder="send a complaint..." className="block p-2.5 text-m text-gray-900  w-2/12 h-24"></textarea>
             </form>
             
             <button className="h-12 px-6 m-2 text-lg  transition-colors duration-150 bg-yellow-500 rounded-lg focus:shadow-outline hover:bg-yellow-400"> Complaint </button>
